da4188deacb463902d97ff4ce05914ac5e3383da
BirisBogdan <biris.bogdan05@gmail.com>
2024-11-08 15:12:17 +0200

    Primul commit site

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <h1 className="justify-center text-3xl font-bold text-green-700 underline">
+      Hello world!
+    </h1>
+  );
+};
+
+export default App;
