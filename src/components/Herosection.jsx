bad004ee1773c9519d9b225cb29c24f3acfd6b0f
BirisBogdan <biris.bogdan05@gmail.com>
2024-11-08 19:15:59 +0200

    Site light mode

--- src/components/Herosection.jsx
+++ src/components/Herosection.jsx
@@ -4,6 +4,7 @@ const Herosection = () => {
       <h1 className="lg:text-12xl sm:text-10xl bg-gradient-to-r from-red-700 to-red-300 bg-clip-text text-center text-9xl tracking-wide text-transparent">
         Mediva
       </h1>
+
       <p className="mt-5 max-w-4xl text-center text-2xl text-neutral-500">
         Your health, our no. 1 priority
       </p>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,21 +3,29 @@ import { navItems } from "../constants";
 
 const navbar = () => {
   return (
-    <nav className="berder-b sticky top-0 z-50 border-neutral-100 py-3 backdrop-blur">
+    <nav className="berder-b sticky top-0 z-50 border-neutral-100 py-4 backdrop-blur">
       <div className="container relative mx-auto px-4 text-sm">
         <div className="flex items-center justify-between">
           <div className="flex flex-shrink-0 items-center">
-            <img className="mr-2 h-10 w-10 rounded-md" src={logo} alt="logo" />
+            <img
+              className="ml-2 mr-2 h-10 w-10 rounded-md"
+              src={logo}
+              alt="logo"
+            />
             <span className="bg-gradient-to-r from-red-700 to-red-300 bg-clip-text text-xl tracking-tight text-transparent">
               Mediva
+              <span className="text-right text-sm font-thin text-neutral-800">
+                {" "}
+                by StackBytes
+              </span>
             </span>
           </div>
-          <ul className="ml-14 mr-2 hidden space-x-12 lg:flex">
+          <ul className="ml-14 mr-2 hidden space-x-8 lg:flex">
             {navItems.map((item, index) => (
               <li key={index}>
                 <a
                   href={item.href}
-                  className="rounded-md border border-red-900 px-3 py-2 hover:bg-red-900"
+                  className="rounded-md border border-red-500 px-3 py-2 hover:bg-red-400"
                 >
                   {item.label}
                 </a>
